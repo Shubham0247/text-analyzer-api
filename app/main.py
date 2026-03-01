0719fde4685722f4d1cf9f426c353408af6678c7
Shubham <shubhamaudattapure47@gmail.com>
2026-03-01 11:59:38 +0530

    Initial commit - Text Analyzer API

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from mangum import Mangum
+import re
+
+app = FastAPI(title="Text Analyzer API")
+
+class TextRequest(BaseModel):
+    text: str
+
+@app.get("/")
+def home():
+    return {"message": "Text Analyzer API Running"}
+
+@app.post("/analyze")
+def analyze_text(data: TextRequest):
+    text = data.text
+    
+    words = len(text.split())
+    characters = len(text)
+    sentences = len(re.findall(r'[.!?]+', text))
+
+    return {
+        "word_count": words,
+        "character_count": characters,
+        "sentence_count": sentences
+    }
+
+# Lambda handler
+handler = Mangum(app)
